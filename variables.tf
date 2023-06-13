464fb3b1d3bb9b902cf63a8d0a8c418c7d677fc1
meghanabollam123 <134513961+meghanabollam123@users.noreply.github.com>
2023-06-13 22:35:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,4 @@
-variable "aws_access_key" {
-default = "AKIASWY3CCFWDYDTNCA2"
-}
-variable "aws_secret_key" {
-default = "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"
- }
+
 variable "region" {
     default = "us-east-1"
 }
