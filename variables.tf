ec2d2491ece7e088fdb8d1b667e4fb6682abc58b
meghanabollam123 <134513961+meghanabollam123@users.noreply.github.com>
2023-06-13 15:44:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {
+default = "AKIASWY3CCFWDYDTNCA2"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "bucket_name" {
+    default = "teesstuserexample"
+}
